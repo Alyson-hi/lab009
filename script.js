0357a906d242b0931bcc8f1b45fbb6e41fc95072
Alyson-hi <anm76@njit.edu>
2024-11-22 23:47:51 -0500

    lab009

---
+++ script.js
@@ -0,0 +1,15 @@
+characters = ['Harry', 'Hermione', 'Ron', 'Draco', 'Luna']
+for (let characters = 1; characters <= 5; characters++) {
+    console.log ("Characters", characters);
+}
+
+let firstName = "Harry"
+let house = "Gryffindor"
+
+let str = "Welcome, Harry of Gryffindor!";
+console.log(str);
+
+let characterName = "Hermione"
+console.log("\ntoUpperCase():", str.toUpperCase());
+console.log("toLowerCase():", str.toLowerCase());
+
